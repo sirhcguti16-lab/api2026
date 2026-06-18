958bc47f9c9b75628ba38d97e7e85a513ed92f55
sirhcguti16-lab <sirhcguti16@gmail.com>
2026-06-18 17:14:36 -0500

    Add files via upload

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -1,18 +1,16 @@
 import { Router } from 'express'
 import { authRequired } from '../middlewares/authMiddleware.js'
-
 import { getClientes, getClientesxid, postinsertarClientes, putClientes, patchClientes, deleteClientes } from '../controladores/clientesCtrl.js'
 
 const router = Router()
 
-// Proteger todas las rutas de clientes con JWT
 router.use(authRequired)
 
-router.get('/clientes',getClientes)
-router.get('/clientes/:id', getClientesxid)
-router.post('/clientes', postinsertarClientes)
-router.put('/clientes/:id', putClientes)
-router.patch('/clientes/:id', patchClientes)
-router.delete('/clientes/:id', deleteClientes)
+router.get('/', getClientes)
+router.get('/:id', getClientesxid)
+router.post('/', postinsertarClientes)
+router.put('/:id', putClientes)
+router.patch('/:id', patchClientes)
+router.delete('/:id', deleteClientes)
 
 export default router
